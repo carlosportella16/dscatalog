05c8295ad3757d8f328ade6711f93cab8a9422f2
Carlos <carlosportella16@hotmail.com>
2022-06-05 17:01:26 -0300

    exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -29,7 +30,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.get();
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
