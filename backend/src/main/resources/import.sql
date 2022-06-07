53057e1d03695f0ff8ad008904fad1960e86631c
Carlos <carlosportella16@hotmail.com>
2022-06-07 19:29:15 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category(name) VALUES ('Books');
-INSERT INTO tb_category(name) VALUES ('Electronics');
-INSERT INTO tb_category(name) VALUES ('Computers');
+INSERT INTO tb_category(name, created_At) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Electronics', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Computers', NOW());
