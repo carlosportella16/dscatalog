9496060c2be1dc48d1cd26329f72979546dd1de5
Carlos <carlosportella16@hotmail.com>
2022-09-21 16:19:36 -0300

    Integration tests with delete id

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServicesIT.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dscatalog.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@SpringBootTest
+public class ProductServicesIT {
+	
+	@Autowired
+	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1l;
+		nonExistingId = 1000l;
+		countTotalProducts = 25l;
+				
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExists() {
+		
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts -1, repository.count());
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+			service.delete(nonExistingId);
+		});
+	}
+}
